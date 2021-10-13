5f7277b4ab117dd28bfe100c5cf09b12aeb47392
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-10-13 16:59:28 -0400

    All REST handlers consider OpenTracing span context.

--- atlas.com/bns/name/resource.go
+++ atlas.com/bns/name/resource.go
@@ -1,80 +1,106 @@
 package name
 
 import (
-	"encoding/json"
+	json2 "atlas-bns/json"
+	"atlas-bns/rest"
 	"github.com/gorilla/mux"
+	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
-	"io"
 	"net/http"
 )
 
-type responseList struct {
-	Data []responseData `json:"data"`
+type dataListContainer struct {
+	Data []dataBody `json:"data"`
 }
 
-type responseData struct {
-	ID         string             `json:"id"`
-	Type       string             `json:"type"`
-	Attributes responseAttributes `json:"attributes"`
+type dataBody struct {
+	ID         string     `json:"id"`
+	Type       string     `json:"type"`
+	Attributes attributes `json:"attributes"`
 }
 
-type responseAttributes struct {
+type attributes struct {
 	Name string `json:"name"`
 }
 
+const (
+	getName  = "get_name"
+	getNames = "get_names"
+)
+
 func InitResource(router *mux.Router, l logrus.FieldLogger) {
-
 	r := router.PathPrefix("/names").Subrouter()
-	r.HandleFunc("", GetName(l)).Methods(http.MethodGet).Queries("name", "{name}")
-	r.HandleFunc("", GetNames(l)).Methods(http.MethodGet)
+	r.HandleFunc("", registerGetName(l)).Methods(http.MethodGet).Queries("name", "{name}")
+	r.HandleFunc("", registerGetNames(l)).Methods(http.MethodGet)
 }
 
-func GetNames(l logrus.FieldLogger) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		vs := GetCache().GetNames()
+func registerGetNames(l logrus.FieldLogger) http.HandlerFunc {
+	return rest.RetrieveSpan(getNames, func(span opentracing.Span) http.HandlerFunc {
+		return handleGetNames(l)(span)
+	})
+}
 
-		var result responseList
-		for _, v := range vs {
-			vr := responseData{
-				ID:         v,
-				Type:       "com.atlas.cos.rest.attribute.BlockedNameAttributes",
-				Attributes: responseAttributes{Name: v},
+func handleGetNames(l logrus.FieldLogger) func(span opentracing.Span) http.HandlerFunc {
+	return func(span opentracing.Span) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			var result dataListContainer
+			for _, v := range GetCache().GetNames() {
+				result.Data = append(result.Data, getDataBody(v))
 			}
-			result.Data = append(result.Data, vr)
-		}
-		rw.WriteHeader(http.StatusOK)
-		err := toJSON(result, rw)
-		if err != nil {
-			l.WithError(err).Errorf("Error writing response")
+			w.WriteHeader(http.StatusOK)
+			err := json2.ToJSON(result, w)
+			if err != nil {
+				l.WithError(err).Errorf("Error writing response")
+			}
 		}
 	}
 }
 
-func GetName(l logrus.FieldLogger) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		name := mux.Vars(r)["name"]
+type nameHandler func(name string) http.HandlerFunc
 
-		var result responseList
-		if val, ok := GetCache().GetName(name); ok {
-			vr := responseData{
-				ID:         val,
-				Type:       "com.atlas.cos.rest.attribute.BlockedNameAttributes",
-				Attributes: responseAttributes{Name: val},
-			}
-			result.Data = append(result.Data, vr)
+func parseName(l logrus.FieldLogger, next nameHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if val, ok := mux.Vars(r)["name"]; ok {
+			next(val)(w, r)
 		} else {
-			result.Data = make([]responseData, 0)
-		}
-		rw.WriteHeader(http.StatusOK)
-		err := toJSON(result, rw)
-		if err != nil {
-			l.WithError(err).Errorf("Error writing response")
+			l.Errorf("Name was not provided to request.")
+			w.WriteHeader(http.StatusBadRequest)
 		}
 	}
 }
 
-// toJSON serializes the given interface into a string based JSON format
-func toJSON(i interface{}, w io.Writer) error {
-	e := json.NewEncoder(w)
-	return e.Encode(i)
+func registerGetName(l logrus.FieldLogger) http.HandlerFunc {
+	return rest.RetrieveSpan(getName, func(span opentracing.Span) http.HandlerFunc {
+		return parseName(l, func(name string) http.HandlerFunc {
+			return handleGetName(l)(span)(name)
+		})
+	})
+}
+
+func handleGetName(l logrus.FieldLogger) func(span opentracing.Span) func(name string) http.HandlerFunc {
+	return func(span opentracing.Span) func(name string) http.HandlerFunc {
+		return func(name string) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				var result dataListContainer
+				if val, ok := GetCache().GetName(name); ok {
+					result.Data = append(result.Data, getDataBody(val))
+				} else {
+					result.Data = make([]dataBody, 0)
+				}
+				w.WriteHeader(http.StatusOK)
+				err := json2.ToJSON(result, w)
+				if err != nil {
+					l.WithError(err).Errorf("Error writing response")
+				}
+			}
+		}
+	}
+}
+
+func getDataBody(name string) dataBody {
+	return dataBody{
+		ID:         name,
+		Type:       "com.atlas.cos.rest.attribute.BlockedNameAttributes",
+		Attributes: attributes{Name: name},
+	}
 }
